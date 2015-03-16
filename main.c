cc8d4440151ffc1416eadfd983c03f9b6499b746
Gezhes Petr <pet.pohta@gmail.com>
2015-03-16 12:53:23 +0300

    Availiable cd and local program launch

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
     int code;
 
     int i = 0;
+    printf("\n");
     for (i = 0; i < 6; ++i)
     {
         printf("\033[%dm*\033[0m", 31 + i);
@@ -22,8 +23,8 @@ int main(int argc, char **argv)
 
     while (1)
     {
-        //printf(">> \033[32m%s:\033[0m\n", getenv("USER"));
-        printf(">> \033[32mUSER:\033[0m%s\n", path);
+        //printf(">> \033[32m%s:\033[0m:%s\n", getenv("USER"), path);
+        printf("[\033[32mUSER\033[0m]:%s>", path);
         code = scanf("%s", s);
 
         if (strcmp(s, "cd") == 0)
@@ -32,13 +33,27 @@ int main(int argc, char **argv)
             scanf("%s", newpath);
             if (newpath[0] == '/')
                 strcpy(path, newpath);
+            else if (strcmp(newpath, "..") == 0)
+            {
+                int len = strlen(path);
+                int newlen = len - 1;
+                while (newlen > 0 && path[newlen] != '/')
+                    --newlen;
+                for (i = newlen; i <= len; ++i)
+                        path[i] = 0;
+                path[0] = '/';
+            }
             else
+            {
+                path[strlen(path)] = '/';
                 strcat(path, newpath);
+            }
             continue;
         }
         if (code == EOF || strcmp(s, "exit") == 0)
         {
             int i = 0;
+            printf("\n");
             for (i = 0; i < 6; ++i)
             {
                 printf("\033[%dm*\033[0m", 31 + i);
@@ -57,10 +72,21 @@ int main(int argc, char **argv)
 
         if (child == 0)
         {
-            sprintf(file_addr, "/bin/%s", s);
-            //file_addr = strcat(file_addr, s);
-            printf("%s: %s\n", path, file_addr);
-            int code = execl(file_addr, file_addr, NULL);
+            int code;
+            if (s[0] == '.')
+            {
+                strcpy(file_addr, path);
+                strcat(file_addr, s + 1);
+                printf("#starting: %s...\n", file_addr);
+                code = execl(file_addr, file_addr, NULL);
+            }
+            else
+            {
+                sprintf(file_addr, "/bin/%s", s);
+                //file_addr = strcat(file_addr, s);
+                printf("%s: %s...\n", path, file_addr);
+                code = execl(file_addr, file_addr, path, NULL);
+            }
 
             if (code == -1)
                 printf("ERROR: errno = %d\n", errno);
