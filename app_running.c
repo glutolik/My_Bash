38e66ef12956604dc102dcaef1fff377934e30b1
Maxim Dankovtsev <max.dankow@gmail.com>
2015-05-12 20:47:39 +0300

    Exit code done

--- app_running.c
+++ app_running.c
@@ -5,31 +5,52 @@ const int IS_NOT_FIRST = 0;
 
 int run_application(int d_in, int d_out, int d_err, const char* app_name, char *const argv[], int* returned_code)
 {
+	int shm = shm_open("/exec_return", O_RDWR | O_CREAT, 0666);
+	if (shm == -1)
+    {
+    	return -1;
+    }
+    ftruncate(shm, sizeof(int));
+    int* exec_return_code = (int*) mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);
+    if (exec_return_code == NULL)
+    {
+    	return -1;
+    }
   //делаем ответвление для запуска
 	pid_t exec_child = fork();
 	if (exec_child == -1)
 	{
-		perror("Can't fork to run app");
 		return -1;
 	}
 	if (exec_child == 0)
 	{
+		*exec_return_code = EXIT_SUCCESS;
 	  //перенаправляем все потоки
 		if ((dup2(d_in, 0) == -1) || (dup2(d_out, 1) == -1) || (dup2(d_err, 2) == -1))
         {
-            perror("dup2 error.");
-            _exit(EXIT_FAILURE);
+        	*exec_return_code = EXIT_FAILURE;
+			exit(EXIT_FAILURE);
         }
   	  //выполняем exec
 		if (execvp(app_name, argv) == -1)
 		{
-			perror("exec");
-			_exit(EXIT_FAILURE);
+			*exec_return_code = EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
 	}
-	wait(NULL);
-  //получаем код возврата
-	printf("All done\n");
+  //получаем код возврата	
+	int exit_status;
+	wait(&exit_status);
+  //если запуск неудачен
+	if (*exec_return_code == EXIT_FAILURE || !WIFEXITED(exit_status))
+	{
+		return -1;
+	}	
+	if (returned_code != NULL)
+	{
+		*returned_code = WEXITSTATUS(exit_status);
+	}
+	return 0;
 }
 
 int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, char *const argv[])
@@ -38,9 +59,11 @@ int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, 
   //если это не последняя команда в цепочке, то создаем pipe
 	if (output_fd == -1)
 	{
-		printf("pipes created for |\n");
 		int pipes[2];
-		pipe(pipes);
+		if (pipe(pipes) == -1)
+		{
+			return -1;
+		}
 		write_fd = pipes[1];
 		read_fd = pipes[0];
 	}
@@ -50,7 +73,10 @@ int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, 
 		write_fd = output_fd;
 		read_fd = -1;
 	}
-	run_application(input_fd, write_fd, 2, app_name, argv, NULL);
+	if (run_application(input_fd, write_fd, 2, app_name, argv, NULL) == -1)
+	{
+		return -1;
+	}
   //если это не первая команда в цепочке, то следует закрыть соединяющий входной пайп   
     if (first != IS_FIRST)
     {
@@ -64,23 +90,17 @@ int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, 
     return read_fd;
 }
 
-int run_comand_chain(int d_in, int d_out, int d_err, int comand_number, 
+int run_comand_chain(int d_in, int d_out, int d_err, int comand_count, 
 	const char** apps_names, char** apps_args[], int* returned_code)
 {
 	int next;
-	for(size_t i = 0; i < comand_number; ++i)
+	for(size_t i = 0; i < comand_count; ++i)
 	{
 	  //если команда последняя, то писать в d_out, иначе в соединяющий пайп	
-		int current_out = (i + 1 == comand_number)? d_out : -1;
-		if (i + 1 == comand_number)
-		{
-			printf("LAST:\n");
-		}
-		printf("%s\n", apps_names[i]);
+		int current_out = (i + 1 == comand_count)? d_out : -1;
 	  //если команда первая, то читать из d_in	
 		if (i == 0)
 		{
-			printf("FIRST\n");
 			next = bind_two_apps(IS_FIRST, d_in, current_out, apps_names[i], apps_args[i]);
 		}
 	  //иначе из соединительного пайпа	
--- app_running.h
+++ app_running.h
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 extern const int IS_FIRST;
 extern const int IS_NOT_FIRST;
@@ -9,5 +13,5 @@ extern const int IS_NOT_FIRST;
 int run_application(int d_in, int d_out, int d_err, const char* app_name, char *const argv[], int* returned_code);
 int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, char *const argv[]);
 
-int run_comand_chain(int d_in, int d_out, int d_err, int comand_number, 
+int run_comand_chain(int d_in, int d_out, int d_err, int comand_count, 
 	const char** apps_names, char** apps_args[], int* returned_code);
