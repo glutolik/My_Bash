f3b60cd510f05a32b73b8ca125bdde0e1bbb4966
Maxim Dankovtsev <max.dankow@gmail.com>
2015-05-14 22:48:28 +0300

    no wait in foreground process + all jobs -> List

--- app_running.h
+++ app_running.h
@@ -11,8 +11,8 @@
 #include <sys/stat.h>
 #include <signal.h>
 
-extern const int IS_FIRST;
-extern const int IS_NOT_FIRST;
+extern const int RUN_BACKGROUND;
+extern const int RUN_FOREGROUND;
 
 typedef struct 
 {
@@ -36,11 +36,11 @@ int run_application(int d_in, int d_out, int d_err, const char* app_name, char *
 int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, char *const argv[]);
 
 int run_comand_chain(int d_in, int d_out, int d_err, int comand_count, 
-	const char** apps_names, char** apps_args[], int* returned_code, JobsList* jobs);
+	const char** apps_names, char** apps_args[], int* returned_code, JobsList* jobs, int bg_flag);
 
 JobsList* init_jobs_system(size_t new_size);
 void delete_jobs_system(JobsList* jobs);
-void add_job(JobsList* jobs, pid_t new_pid, char* name, int* real_fake_fd, int new_current_in);
+void add_job(JobsList* jobs, pid_t new_pid, char* name, int* real_fake_fd, int new_current_in, int new_bg_flag);
 void show_jobs(JobsList* jobs);
 int* create_fake_discriptor(void);
 void delete_fake_discriptor(int* fd);
@@ -48,4 +48,5 @@ int signal_process(JobsList* jobs, size_t job_number, int signal_to_send);
 int stop_process(JobsList* jobs, size_t job_number);
 int continue_process(JobsList* jobs, size_t job_number);
 int process_to_foreground(JobsList* jobs, size_t job_number);
+pid_t get_active_pid(JobsList* jobs);
 #endif
