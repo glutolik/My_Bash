f163be072c271912fae45fabf078f10beb8dd249
Maxim Dankovtsev <max.dankow@gmail.com>
2015-05-17 18:10:46 +0300

    NOT TESTED. many active pipes

--- app_running.h
+++ app_running.h
@@ -19,9 +19,7 @@ typedef struct
 	pid_t pid;
 	int status;
 	int fg_flag;
-	int current_input;
-	int real_input;
-	int* fake_fd;
+	int* fd;
 	char* comand_str;
 } JobStruct;
 
@@ -36,12 +34,12 @@ int run_application(int d_in, int d_out, int d_err, const char* app_name, char *
 	int* returned_code, JobsList* jobs);
 int bind_two_apps(int first, int input_fd, int output_fd, const char* app_name, char *const argv[]);
 
-int run_comand_chain(int d_in, int d_out, int d_err, int pipe_write, int comand_count, 
-	const char** apps_names, char** apps_args[], int* returned_code, JobsList* jobs, int bg_flag);
+int run_comand_chain(int d_in, int d_out, int d_err, int comand_count, const char** apps_names, 
+	char** apps_args[], int* returned_code, JobsList* jobs, int bg_flag);
 
 JobsList* init_jobs_system(size_t new_size);
 void delete_jobs_system(JobsList* jobs);
-void add_job(JobsList* jobs, pid_t pid, char* name, int* fake_fd, int current_in, int real_in, int fg_flag);
+void add_job(JobsList* jobs, pid_t pid, char* name, int* new_fd, int fg_flag);
 void show_jobs(JobsList* jobs);
 int* create_fake_discriptor(void);
 void delete_fake_discriptor(int* fd);
@@ -51,5 +49,6 @@ int continue_process(JobsList* jobs, size_t job_number);
 int process_to_foreground(JobsList* jobs, size_t job_number);
 int process_to_background(JobsList* jobs, size_t job_number);
 pid_t get_active_pid(JobsList* jobs);
+int get_active_fd(JobsList* jobs);
 ssize_t pid_to_job_number(JobsList* jobs, pid_t pid);
 #endif
